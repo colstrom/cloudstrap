348dbaf2bfe8b9a459d40f2937226cdad1bf1d00
Chris Olstrom <chris@olstrom.com>
2016-08-29 16:07:32 -0700

    Add SSH::Client class

---
+++ lib/stackato-lkg/ssh/client.rb
@@ -0,0 +1,36 @@
+require 'contracts'
+require 'sshkit'
+
+module StackatoLKG
+  module SSH
+    class Client
+      include ::Contracts::Core
+      include ::Contracts::Builtin
+
+      Contract String => Client
+      def initialize(private_key)  # TODO: Eliminate side-effects
+        ::SSHKit::Backend::Netssh.configure do |ssh|
+          ssh.ssh_options = {
+            config: false,
+            auth_methods: ['publickey'],
+            keys: private_key
+          }
+        end
+
+        self
+      end
+
+      Contract String, Proc => Any
+      def to(host, &block)
+        ::SSHKit::Coordinator.new("#{config.ssh_username}@#{host}").each(&block)
+      end
+
+      private
+
+      Contract None => ::StackatoLKG::Config
+      def config
+        @config ||= ::StackatoLKG::Config.new
+      end
+    end
+  end
+end
