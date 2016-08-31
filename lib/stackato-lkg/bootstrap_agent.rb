24ef46a89c9672561dad2a4f1e796f636c9803a1
Chris Olstrom <chris@olstrom.com>
2016-08-31 13:31:43 -0700

    Log bootstrap output for troubleshooting.

--- lib/stackato-lkg/bootstrap_agent.rb
+++ lib/stackato-lkg/bootstrap_agent.rb
@@ -350,7 +350,7 @@ module StackatoLKG
 
       ssh.to(jumpbox_ip) do
         with(aws_access_key_id: access_key_id, aws_secret_access_key: secret_access_key) do
-          execute(:bootstrap, :install, 'bootstrap.properties')
+          execute :bootstrap, *%w(install bootstrap.properties 2>&1 | tee bootstrap.log)
         end
       end
     end
