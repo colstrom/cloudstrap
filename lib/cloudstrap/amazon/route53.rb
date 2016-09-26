598d81fb422981fb6f85d0de8aa1c4292e6b92e0
Chris Olstrom <chris@olstrom.com>
2016-09-26 11:36:36 -0700

    Add methods to find zones

--- lib/cloudstrap/amazon/route53.rb
+++ lib/cloudstrap/amazon/route53.rb
@@ -8,6 +8,16 @@ module Cloudstrap
       include ::Contracts::Core
       include ::Contracts::Builtin
 
+      Contract None => ArrayOf[::Aws::Route53::Types::HostedZone]
+      def zones
+        @zones ||= zones!
+      end
+
+      Contract None => ArrayOf[::Aws::Route53::Types::HostedZone]
+      def zones!
+        @zones = call_api(:list_hosted_zones).hosted_zones
+      end
+
       private
 
       def client
