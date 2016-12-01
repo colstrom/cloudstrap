26818e868f4c8ae08b9decda2667c9317c4e5fb7
Chris Olstrom <chris@olstrom.com>
2016-12-01 12:01:26 -0800

    Add default for hdp_bootstrap_version

--- lib/cloudstrap/config.rb
+++ lib/cloudstrap/config.rb
@@ -75,7 +75,10 @@ module Cloudstrap
 
     Contract None => String
     def hdp_bootstrap_version
-      lookup :hdp_bootstrap_version
+      lookup(:hdp_bootstrap_version) do
+        STDERR.puts pastel.yellow '# No version given, using default release'
+        '1.0.20-0-gda74de0'
+      end
     end
 
     alias hdp_version hdp_bootstrap_version
