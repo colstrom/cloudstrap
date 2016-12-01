93e941588c4cce8fa0986274aa7e0811064a98c7
Chris Olstrom <chris@olstrom.com>
2016-12-01 15:48:27 -0800

    properties_seed_url is no longer required

--- lib/cloudstrap/config.rb
+++ lib/cloudstrap/config.rb
@@ -94,7 +94,7 @@ module Cloudstrap
 
     Contract None => String
     def properties_seed_url
-      required :properties_seed_url
+      lookup(:properties_seed_url) { '' }
     end
 
     Contract None => String
